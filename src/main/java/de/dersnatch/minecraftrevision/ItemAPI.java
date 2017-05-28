f25f467fdcc70aa95d3c59b02c3f4e830859ceb6
DerSnatch <snatchoriginal@gmail.com>
2017-05-28 09:41:01 +0200

    Initial commit

---
+++ src/main/java/de/dersnatch/minecraftrevision/ItemAPI.java
@@ -0,0 +1,27 @@
+package de.dersnatch.minecraftrevision;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Copyright (c) 2017 Minecraftrevision.de
+ * Licensed under GNU General Public License, version 3 (GPL-3.0)
+ *
+ * @author Schule
+ * @version ${VERSION}
+ *
+ * Project created on 28.05.2017-09:19
+ **/
+
+public class ItemAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
